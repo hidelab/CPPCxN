9767b0efd5d02a3cbcf57bcd80538639ce4b675f
BioSok <sokratiskariotis@gmail.com>
2018-07-02 10:43:12 +0100

    Adding estimates 00

---
+++ Improved PCxN/CPP_mean_pcor2_barcode_hallmark_estimates00.R
@@ -0,0 +1,23 @@
+# Sokratis: Part of improved PCxN
+# Update Gene Set Annotation: Keep only genes present in the gene expression background
+
+rm(list=ls())
+options(stringsAsFactors = F)
+
+# ==== Inputs ====
+genesets <- DPD.Hs.gs.mini.PDN.RDS
+
+# ==== Desired Gene Sets ===
+# Gene set annotation from MSigDB
+fname = paste0("../data/", genesets, sep = "")
+h_gs = readRDS(fname)
+
+# ==== Background Genes ====
+# Gene present in the microarrays from the gene expression background
+background_genes = readRDS("../data/barcode_genes.RDS")
+
+# for each gene set annotation, keep only the genes present in the gene expression background
+my_gs = lapply(h_gs,function(x){x[x %in% background_genes]})
+
+# save filtered gene set annotations
+saveRDS(my_gs,fname)
