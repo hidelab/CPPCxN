967643819553c4af3ab8868ab998f2617f741bf0
BioSok <sokratiskariotis@gmail.com>
2018-07-10 12:43:34 +0100

    About combinations

---
+++ Improved PCxN/Combiner/Pcombiner_sharc.R
@@ -0,0 +1,55 @@
+# Aggregate results into a single data frame
+# We need estimates02 to be completed for both matrices
+
+# How to use
+# Place parts1 and 2 (output of estimates 2) of both matrices in the same folder as this script.
+# As the first argument use the matrix names (the part before "_part1.RDS"). As the second argument 
+# use the entire name of the combined matrix (e.g. matri1_matrix2.RDS)
+# Example (files already in folder):
+# Rscript Pcombiner_sharc.R pcxn_conc_base pcxn_conc_plus_10 pcxn_conc_plus_20 all_concs.RDS
+
+rm(list = ls(all=TRUE)) 
+options(stringsAsFactors = F)
+
+# ==== Arguments ====
+# command line arguments from submission (sharc) script
+# 1 - genesets file
+cmd_args <- commandArgs(trailingOnly = T)
+
+# ==== INPUTS ====
+inputs <- c("pcxn_conc_base","pcxn_conc_plus_10","pcxn_conc_plus_20")
+output_name <- "all_concs"
+# Check if all necessary files exists in this directory
+for(i in inputs) {
+    tname1 <- paste(i,"_part1.RDS",sep = "")
+    tname2 <- paste(i,"_part2.RDS",sep = "")
+    
+    if(!file_test("-f", tname1)) stop(paste(tname1, ": No such file found in this directory!",sep = ""))
+    if(!file_test("-f", tname2)) stop(paste(tname2, ": No such file found in this directory!",sep = ""))
+}
+
+output_folder <- "conc_output_mini_PDN"
+
+# empty variable to store results
+pcxn = c()
+pb = txtProgressBar(min=0,max=2,initial=0,style=3)
+
+for(k in 1:2){
+    # read each part of each
+    conc_tmp <- c()
+    for (m in inputs) {
+        tmp <- readRDS(paste0(m ,"_part", k, ".RDS"))
+        conc_tmp <- rbind(conc_tmp,tmp)
+    }
+    
+    conc_tmp <- unique(conc_tmp)
+    pcxn = rbind(pcxn, conc_tmp)
+    setTxtProgressBar(pb,k)
+}
+close(pb)
+
+# adjust p-values for multiple comparison
+pcxn$p.Adjust = p.adjust(p = pcxn$p.value, method = "fdr")
+
+# save results
+saveRDS(pcxn, paste0(output_name,".RDS"))
