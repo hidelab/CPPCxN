de1332d1bccd3eb14287eb2ce1fdfa1728bea809
BioSok <sokratiskariotis@gmail.com>
2018-07-16 15:33:05 +0100

    Add converter to imporved PCxN

--- Improved PCxN/improved_PCxN_estimates03.R
+++ Improved PCxN/improved_PCxN_estimates03.R
@@ -26,3 +26,23 @@ pcxn$p.Adjust = p.adjust(p = pcxn$p.value, method = "fdr")
 
 # save results
 saveRDS(pcxn, paste0("../",output_folder,"/improved_PCxN_",geneset_file))
+
+original_matrix <- pcxn
+
+unique_pathways <- unique(c(original_matrix$Pathway.A,original_matrix$Pathway.B))
+pathway_number <- length(unique_pathways)
+rns <- unique_pathways
+cns <- unique_pathways
+
+square_matrix <- matrix(, pathway_number, pathway_number, dimnames = list(rns, cns))
+
+for(row in 1:nrow(original_matrix)) {
+    tp1 <- original_matrix[row, 1]
+    tp2 <- original_matrix[row, 2]
+    square_matrix[tp1,tp2] <- original_matrix[row, 4]
+    square_matrix[tp2,tp1] <- original_matrix[row, 4]
+}
+
+diag(square_matrix) <- "1"
+
+saveRDS(pcxn, paste0("../",output_folder,"/square_improved_PCxN_",geneset_file))
